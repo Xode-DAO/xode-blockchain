4b91b411f7ea3a074360fb89ec62bc03da9bcedb
hgminerva <hgminerva@gmail.com>
2024-09-20 12:07:23 +0900

    Add automation - upload binary

--- automation/xode-node-upload.sh
+++ automation/xode-node-upload.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# https://www.dropbox.com/developers/apps
+# https://stackoverflow.com/questions/42120767/upload-file-on-linux-cli-to-dropbox-via-bash-sh
+
 curl -X POST https://content.dropboxapi.com/2/files/upload \
     --header "Authorization: Bearer $ACCESS_TOKEN" \
     --header "Dropbox-API-Arg: {\"path\": \"/aarch64/xode-node\", \"mode\": \"overwrite\", \"strict_conflict\": false}" \
